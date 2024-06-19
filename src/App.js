102a4f2d71f61a7d5a41cd99f2b647cefa660151
Ephriam Kassa <ephriam123@gmail.com>
2024-06-19 08:31:57 -0700

    sdadsadaeee

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function Carousel({ radius = 1.4, count = 8 }) {
   return Array.from({ length: count }, (_, i) => (
     <Card
       key={i}
-      url={`/img${Math.floor(i % 10) + 1}_.jpg`}
+      url={`/img${Math.floor(i % 10) + 1}_.webp`}
       position={[Math.sin((i / count) * Math.PI * 2) * radius, 0, Math.cos((i / count) * Math.PI * 2) * radius]}
       rotation={[0, Math.PI + (i / count) * Math.PI * 2, 0]}
     />
